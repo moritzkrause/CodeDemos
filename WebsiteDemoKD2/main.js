ad71078b622a21a671e743d025538f1134a59ff3
mkrause4 <moritz.krause.2@iisys.de>
2025-04-16 11:33:21 +0200

    new file:   .DS_Store 	new file:   WebsiteDemoKD2/.DS_Store 	new file:   WebsiteDemoKD2/WebsiteDemoKD2.zip 	new file:   WebsiteDemoKD2/assets/main.css 	new file:   WebsiteDemoKD2/fonts/fonts.otf 	new file:   WebsiteDemoKD2/index.html 	new file:   WebsiteDemoKD2/main.js

---
+++ WebsiteDemoKD2/main.js
@@ -0,0 +1,6 @@
+//Javascript
+
+/**
+ * In Javascript bestimmt ihr das Verhalten eurer Website auf Eingaben, die Zeit, Daten, ...
+ * Hier könnt ihr auch mit externen Libraries wie p5js arbeiten.
+ */
